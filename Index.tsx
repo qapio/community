8888724ca71baf9740ea6c62cbb502f838d052d2
Ariel Fischer <arieldf@gmail.com>
2025-04-24 13:50:23 +0200

    Update Index.tsx

--- Index.tsx
+++ Index.tsx
@@ -1,2 +1,2 @@
 
-export const Index = () => <div>HEKKkkdfgsdfsdfsdfgggggsdfggggdfsdfsdfdfdgdgfgdgfdgE</div>
+export const Index = () => <div>HEKKkkdfgsdfsdfsdfgggggdfgdgdgdfgsdfggggdfsdfsdfdfdgdgfgdgfdgE</div>
